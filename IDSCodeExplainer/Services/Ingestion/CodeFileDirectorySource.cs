1cff39b252aad0aca8029edad71e9744f625eec1
justinwcy <pythonemail20@gmail.com>
2025-09-10 13:29:40 +0800

    Change from Data2 to Data folder name

---
+++ IDSCodeExplainer/Services/Ingestion/CodeFileDirectorySource.cs
@@ -0,0 +1,84 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using CodeExplainer.Services;
+using CodeExplainer.Services.Ingestion;
+
+namespace IDSCodeExplainer.Services.Ingestion;
+
+public class CodeFileDirectorySource(string sourceDirectory) : IIngestionSource
+{
+    public string SourceFileId(string path) => Path.GetRelativePath(sourceDirectory, path);
+
+    public string SourceId => $"{nameof(CodeFileDirectorySource)}:{sourceDirectory}";
+
+    public string SourceFileHashsum(string filePath)
+    {
+        using var md5 = MD5.Create();
+        using var stream = File.OpenRead(filePath);
+        return Encoding.Default.GetString(md5.ComputeHash(stream));
+    }
+
+    public Task<IEnumerable<IngestedDocument>> GetNewOrModifiedDocumentsAsync(IReadOnlyList<IngestedDocument> existingDocuments)
+    {
+        var results = new List<IngestedDocument>();
+        var sourceFiles = Directory.GetFiles(sourceDirectory, "*.cs", SearchOption.AllDirectories);
+        var existingDocumentsById = existingDocuments.ToDictionary(d => d.DocumentId);
+
+        foreach (var sourceFile in sourceFiles)
+        {
+            var sourceFileId = SourceFileId(sourceFile);
+            var sourceFileVersion = SourceFileHashsum(sourceFile);
+            var existingDocumentVersion = existingDocumentsById.TryGetValue(sourceFileId, out var existingDocument) ? existingDocument.DocumentVersion : null;
+            if (existingDocumentVersion != sourceFileVersion)
+            {
+                results.Add(new() { Key = Guid.CreateVersion7().ToString(), SourceId = SourceId, DocumentId = sourceFileId, DocumentVersion = sourceFileVersion });
+            }
+        }
+
+        return Task.FromResult((IEnumerable<IngestedDocument>)results);
+    }
+
+    public Task<IEnumerable<IngestedDocument>> GetDeletedDocumentsAsync(IReadOnlyList<IngestedDocument> existingDocuments)
+    {
+        var currentFiles = Directory.GetFiles(sourceDirectory, "*.cs", SearchOption.AllDirectories);
+        var currentFileIds = currentFiles.ToLookup(SourceFileId);
+        var deletedDocuments = existingDocuments.Where(d => !currentFileIds.Contains(d.DocumentId));
+        return Task.FromResult(deletedDocuments);
+    }
+
+    public Task<IEnumerable<IngestedChunk>> CreateChunksForDocumentAsync(IngestedDocument document)
+    {
+        var filePath = Path.Combine(sourceDirectory, document.DocumentId);
+        var codeString = File.ReadAllText(filePath);
+        var recursiveCodeSplitter = new RecursiveCodeSplitter(200, 20);
+        var splitCodeStrings = recursiveCodeSplitter.SplitText(codeString);
+        var chunks = splitCodeStrings.Select(splitCodeString => new IngestedChunk()
+        {
+            Key = Guid.CreateVersion7().ToString(),
+            DocumentId = document.DocumentId,
+            Text = splitCodeString
+        });
+
+        //var lines = File.ReadAllLines(filePath);
+        //var chunks = new List<IngestedChunk>();
+        //int chunkSize = 200;
+        //int chunkIndex = 0;
+
+        //for (int i = 0; i < lines.Length; i += chunkSize)
+        //{
+        //    var chunkLines = lines.Skip(i).Take(chunkSize);
+        //    var chunkText = string.Join(Environment.NewLine, chunkLines);
+
+        //    chunks.Add(new IngestedChunk
+        //    {
+        //        Key = Guid.CreateVersion7().ToString(),
+        //        DocumentId = document.DocumentId,
+        //        Text = chunkText
+        //    });
+
+        //    chunkIndex++;
+        //}
+
+        return Task.FromResult((IEnumerable<IngestedChunk>)chunks);
+    }
+}
---
+++ IDSCodeExplainer/Services/Ingestion/IngestedChunk.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.VectorData;
+
+namespace IDSCodeExplainer.Services.Ingestion;
+
+public class IngestedChunk
+{
+    // 1536 is the default vector size for the OpenAI text-embedding-3-small model
+    // 768 is the default vector size for the nomic-embed-text:latest
+    private const int VectorDimensions = 768; 
+    private const string VectorDistanceFunction = DistanceFunction.CosineDistance;
+
+    [VectorStoreKey]
+    public required string Key { get; set; }
+
+    [VectorStoreData(IsIndexed = true)]
+    public required string DocumentId { get; set; }
+
+    [VectorStoreData]
+    public required string Text { get; set; }
+
+    [VectorStoreVector(VectorDimensions, DistanceFunction = VectorDistanceFunction)]
+    public string? Vector => Text;
+}
